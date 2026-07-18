351765ca941a46c874e15eae054e7bc9475f6ec4
ranausman020000-code <ranausman020000@gmail.com>
2026-07-17 20:08:19 -0700

    Fix Google OAuth strategy lint errors

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -1,36 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Profile, Strategy } from 'passport-google-oauth20';
+
 import { env } from '../../../config/env';
+import { GoogleProfile } from '../interfaces/google-profile.interface';
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-    constructor() {
-        super({
-            clientID: env.google.clientId!,
-            clientSecret: env.google.clientSecret!,
-            callbackURL: env.google.callbackUrl!,
-            scope: ['email', 'profile'],
-        });
-    }
+  constructor() {
+    super({
+      clientID: env.google.clientId!,
+      clientSecret: env.google.clientSecret!,
+      callbackURL: env.google.callbackUrl!,
+      scope: ['email', 'profile'],
+    });
+  }
 
-    async validate(
-        accessToken: string,
-        refreshToken: string,
-        profile: any,
-        done: VerifyCallback,
-    ): Promise<any> {
-        void accessToken;
-        void refreshToken;
+  validate(
+    _accessToken: string,
+    _refreshToken: string,
+    profile: Profile,
+  ): GoogleProfile {
+    const email = profile.emails?.[0]?.value;
 
-        const user = {
-            email: profile.emails?.[0]?.value,
-            firstName: profile.name?.givenName,
-            lastName: profile.name?.familyName,
-            picture: profile.photos?.[0]?.value,
-            googleId: profile.id,
-        };
-
-        done(null, user);
-    }
+    return {
+      googleId: profile.id,
+      email,
+      firstName: profile.name?.givenName,
+      lastName: profile.name?.familyName,
+      picture: profile.photos?.[0]?.value,
+    };
+  }
 }
